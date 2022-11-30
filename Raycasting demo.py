20cb52ae694e26403d3ba9b0edbe15153028e88e
Andrew2026 <118215385+Andrew2026@users.noreply.github.com>
2022-11-29 21:49:45 -0500

    Update Raycasting demo.py

--- Raycasting demo.py
+++ Raycasting demo.py
@@ -14,71 +14,71 @@ def roundHalfUp(d):
     # https://docs.python.org/3/library/decimal.html#rounding-modes
     return int(decimal.Decimal(d).to_integral_value(rounding=rounding))
 
-maze = [['w', 'c', 'w', 'w', 'w', 'w', 'w', 'w'],
-        ['w', 'c', 'c', 'w', 'w', 'c', 'c', 'w'], 
-        ['w', 'w', 'c', 'w', 'w', 'c', 'w', 'w'], 
+maze = [['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
         ['w', 'c', 'c', 'c', 'c', 'c', 'c', 'w'], 
-        ['w', 'c', 'c', 'c', 'w', 'c', 'c', 'w'], 
-        ['w', 'c', 'w', 'w', 'w', 'w', 'c', 'w'], 
-        ['w', 'c', 'c', 'c', 'w', 'c', 'c', 'w'], 
-        ['w', 'w', 'w', 'w', 'w', 'w', 'c', 'w']]
+        ['w', 'c', 'w', 'c', 'w', 'c', 'c', 'w'], 
+        ['w', 'c', 'w', 'c', 'w', 'c', 'w', 'w'], 
+        ['w', 'c', 'c', 'c', 'c', 'c', 'w', 'w'], 
+        ['w', 'c', 'w', 'c', 'w', 'c', 'c', 'w'], 
+        ['w', 'c', 'c', 'c', 'c', 'c', 'c', 'w'], 
+        ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w']]
 
 
 def appStarted(app):
+    app.maze = maze
     app.dimensions = 8
     app.cellSize = 100
-    app.maze = maze
+    app.mazeXO = app.width/2 // app.dimensions
+    app.mazeYO = app.height // app.dimensions
     app.px = 150
     app.py = 150
     app.pdx = 0
     app.pdy = 0
-    app.mazeXO = app.width // app.dimensions
-    app.mazeYO = app.height // app.dimensions
     app.pa = 1.05
     app.playerR = 5
 
 def keyPressed(app, event):
     #move
-    if (event.key == 'a'):
+    if (event.key == 'Left'):
         app.px -= 5
         if not isLegal(app):
             app.px += 5
-    if (event.key == 'd'):
+    if (event.key == 'Right'):
         app.px += 5
         if not isLegal(app):
             app.px -= 5
-    if (event.key == 'w'):
+    if (event.key == 'Up'):
         app.py -= 5
         if not isLegal(app):
             app.py += 5
-    if (event.key == 's'):
+    if (event.key == 'Down'):
         app.py += 5
         if not isLegal(app):
             app.py -= 5
 
-    #look around
-    if (event.key == 'Left'):
+    #look around and move at angle
+    if (event.key == 'a'):
         app.pa -= 0.1
-        if app.pa <= 0:
+        if app.pa < 0:
             app.pa += 2 * math.pi
         app.pdx = math.cos(app.pa) * 5
         app.pdy = math.sin(app.pa) * 5
 
-    if (event.key == 'Right'):
+    if (event.key == 'd'):
         app.pa += 0.1
         if app.pa > 2 * math.pi:
             app.pa -= 2 * math.pi
         app.pdx = math.cos(app.pa) * 5
         app.pdy = math.sin(app.pa) * 5
 
-    if (event.key == 'Up'):
+    if (event.key == 'w'):
         app.px += app.pdx
         app.py += app.pdy
         if not isLegal(app):
             app.px -= app.pdx
             app.py -= app.pdy
 
-    if (event.key == 'Down'):
+    if (event.key == 's'):
         app.px -= app.pdx
         app.py -= app.pdy
         if not isLegal(app):
@@ -94,12 +94,11 @@ def isLegal(app):
         return False
     return True
 
-def drawPlayer(app, canvas):
-    canvas.create_rectangle(app.px-app.playerR, app.py-app.playerR,
-                       app.px+app.playerR, app.py+app.playerR,
-                       fill='darkGreen')
-    canvas.create_line(app.px, app.py, app.px+app.pdx * 5, app.py+app.pdy * 5,
-    fill = 'black', width = 2)
+def getDistance(px, py, rx, ry):
+    return math.sqrt((rx - px)**2 + (ry - py)**2)
+
+def drawBackground(app, canvas):
+    canvas.create_rectangle(0, 0, app.width, app.height, fill = 'black')
 
 def drawMaze(app, canvas):
     for i in range(len(app.maze)):
@@ -114,207 +113,189 @@ def drawMaze(app, canvas):
                 else:
                     canvas.create_rectangle(x0,y0,x1,y1,fill = 'white')
 
-def checkHorizontal(app, canvas):
-    xo, yo, rx, ry, dof = 0, 0, 0, 0, 0
-    ra = app.pa     
+def drawPlayer(app, canvas):
+    canvas.create_rectangle(app.px - app.playerR, app.py - app.playerR,
+                       app.px + app.playerR, app.py + app.playerR,
+                       fill = 'darkGreen')
+    canvas.create_line(app.px, app.py, app.px + app.pdx * 5, app.py + 
+    app.pdy * 5, fill = 'black', width = 2)
+    
+def checkHorizontal(app, ra):
+    xo, yo, rx, ry, cellsChecked, distH = 0, 0, 0, 0, 0, 0
     aTan = -1/math.tan(ra)
+
     #looking up
     if ra > math.pi:
         ry = int(app.py // app.mazeYO) * app.mazeYO
         rx = (app.py - ry) * aTan + app.px
         yo = -app.mazeYO
         xo = -yo * aTan
+
     #looking down
     if ra < math.pi:
-        ry = (int(app.py // app.mazeYO)+1) * app.mazeYO
+        ry = (int(app.py // app.mazeYO) + 1) * app.mazeYO
         rx = (app.py - ry) * aTan + app.px
         yo = app.mazeYO
         xo = -yo * aTan
+    
+    #can't hit horizontal wall
     if ra == 0 or ra == math.pi:
         rx = app.px
         ry = app.py
-        dof = app.dimensions
-    while dof < app.dimensions:
+        cellsChecked = app.dimensions
+    
+    #while ray did not hit wall
+    while cellsChecked < app.dimensions:
         rayRow = int(ry // app.mazeYO)
         rayCol = int(rx // app.mazeXO)
+
         #looking down
         if ra < math.pi:
-
-            if (rayRow < app.dimensions and rayCol < app.dimensions and rayRow >= 0 
-            and rayCol >= 0 and app.maze[rayRow][rayCol]=='w'):
-                dof = app.dimensions
-    
+            if (rayRow < app.dimensions and rayCol < app.dimensions and 
+            rayRow >= 0 and rayCol >= 0 and app.maze[rayRow][rayCol] == 'w'):
+                cellsChecked = app.dimensions
+                distH = getDistance(app.px, app.py, rx, ry)
             else:
                 rx += xo
                 ry += yo
-                dof += 1
+                cellsChecked += 1
+
         #looking up
         else:
-            if (rayRow < app.dimensions and rayCol < app.dimensions and rayRow >= 0 
-            and rayCol >= 0 and app.maze[rayRow-1][rayCol]=='w'):
-                dof = app.dimensions
-                
+            if (rayRow < app.dimensions and rayCol < app.dimensions and 
+            rayRow >= 0 and rayCol >= 0 and app.maze[rayRow-1][rayCol]=='w'):
+                cellsChecked = app.dimensions
+                distH = getDistance(app.px, app.py, rx, ry)
             else:
                 rx += xo
                 ry += yo
-                dof += 1
-    
-    canvas.create_line(app.px, app.py, rx, ry, fill = 'blue', width = 5)
+                cellsChecked += 1
 
-def checkVertical(app, canvas):
-    xo, yo, rx, ry, dof = 0, 0, 0, 0, 0
-    ra = app.pa     
+    return (distH, rx, ry)
+    
+
+def checkVertical(app, ra):
+    xo, yo, rx, ry, cellsChecked, distV = 0, 0, 0, 0, 0, 0   
     nTan = -math.tan(ra)
+
     #looking left
     if ra > math.pi/2 and ra < 3*math.pi/2:
         rx = int(app.px // app.mazeXO) * app.mazeXO
         ry = (app.px - rx) * nTan + app.py
         xo = -app.mazeXO
         yo = -xo * nTan
+
     #looking right
     if ra < math.pi/2 or ra > 3*math.pi/2:
-        rx = (int(app.px // app.mazeXO)+1) * app.mazeXO
+        rx = (int(app.px // app.mazeXO) + 1) * app.mazeXO
         ry = (app.px - rx) * nTan + app.py
         xo = app.mazeXO
         yo = -xo * nTan
+    
+    #can't hit verticle wall
     if ra == 0 or ra == math.pi:
         rx = app.px
         ry = app.py
-        dof = app.dimensions
-    while dof < app.dimensions:
+        cellsChecked = app.dimensions
+    
+    #while ray did not hit wall
+    while cellsChecked < app.dimensions:
         rayRow = int(ry // app.mazeYO)
         rayCol = int(rx // app.mazeXO)
+
         #looking left
         if ra > math.pi/2 and ra < 3*math.pi/2:
-            if (rayRow < app.dimensions and rayCol < app.dimensions and rayRow >= 0 
-            and rayCol >= 0 and app.maze[rayRow][rayCol-1]=='w'):
-                dof = app.dimensions
-                
+            if (rayRow < app.dimensions and rayCol < app.dimensions and 
+            rayRow >= 0 and rayCol >= 0 and app.maze[rayRow][rayCol-1] == 'w'):
+                cellsChecked = app.dimensions
+                distV = getDistance(app.px, app.py, rx, ry)
             else:
                 rx += xo
                 ry += yo
-                dof += 1
+                cellsChecked += 1
+        
+        #looking right
         else:
-            if (rayRow < app.dimensions and rayCol < app.dimensions and rayRow >= 0 
-            and rayCol >= 0 and app.maze[rayRow][rayCol]=='w'):
-                dof = app.dimensions
-                
+            if (rayRow < app.dimensions and rayCol < app.dimensions and 
+            rayRow >= 0 and rayCol >= 0 and app.maze[rayRow][rayCol] == 'w'):
+                cellsChecked = app.dimensions
+                distV = getDistance(app.px, app.py, rx, ry)
             else:
                 rx += xo
                 ry += yo
-                dof += 1
-    canvas.create_line(app.px, app.py, rx, ry, fill = 'red', width = 2)
+                cellsChecked += 1
+    return (distV, rx, ry)
 
 def drawRays(app, canvas):
-    checkHorizontal(app, canvas)
-    checkVertical(app, canvas)
+    #sDist = shortest Ray
+    sDist = 0
 
-def checkHorizontalWalls(app, canvas):
-    xo, yo, rx, ry, dof = 0, 0, 0, 0, 0
-    ra = app.pa      
+    #set ray at 30 degree angle left of player
+    ra = app.pa - math.pi/6
 
-    if ra == 0 or ra == math.pi:
-        ry = app.py
-        rx = app.px
-        dof = app.dimensions
-    #looking Down
-    elif ra < math.pi:
-        ry = ((int(app.py // app.mazeYO))+1) * app.mazeYO
-        if ra < math.pi/2:
-            xo = (ry - app.py)/(math.tan(ra))
-        elif ra == math.pi/2:
-            xo = 0
-        else:
-            xo = ((app.py - ry)*math.tan(ra-(math.pi/2)))
-        rx = (xo + app.px)
-        yo = app.mazeYO
-    #looking Up
-    else:
-        ry = int(app.py // app.mazeYO) * app.mazeYO
-        if ra < (3*math.pi/2):
-            xo = (ry - app.py)/(math.tan(ra-(math.pi)))
-        elif ra == (3*math.pi/2):
-            xo = 0
-        else:
-            xo = ((app.py-ry) * (math.tan(ra-(3*math.pi/2))))
-        rx = (xo + app.px)
-        yo = -app.mazeYO
-    
-    while dof < app.dimensions:
-        rayRow = int(ry // app.mazeYO)
-        rayCol = int(rx // app.mazeXO)
-        if ra > math.pi:
-            if (rayRow < app.dimensions and rayCol < app.dimensions and 
-            app.maze[rayRow-1][rayCol] == 'w'):
-                dof = app.dimensions
-                
-            else:
-                rx += xo
-                ry += yo
-                dof += 1
-        else:
-            if (rayRow < app.dimensions and rayCol < app.dimensions and 
-            app.maze[rayRow][rayCol] == 'w'):
-                dof = app.dimensions
-            else:
-                rx += xo
-                ry += yo
-                dof += 1
-    canvas.create_line(app.px, app.py, rx, ry, fill = 'blue', width = 3)
+    #check bounds
+    if ra < 0:
+            ra += 2*math.pi
+    if ra > 2*math.pi:
+            ra -= 2*math.pi
 
-def checkVerticalWalls(app, canvas):
-    xo, yo, rx, ry, dof = 0, 0, 0, 0, 0
-    ra = app.pa
-    #looking Right
-    if ra < math.pi/2 or ra > (3 * math.pi/2):
-        rx = (int(app.px // app.mazeXO)+1) * app.mazeXO
-        if ra == 0:
-            yo = 0
-        elif ra < math.pi/2:
-            yo = (rx - app.px)/(math.tan(ra))
+    for ray in range(60):
+        (distH, xH, yH) = checkHorizontal(app, ra)
+        (distV, xV, yV) = checkVertical(app, ra)
+        if distH == 0:
+            canvas.create_line(app.px, app.py, xV, yV, fill = 'green', width = 2)
+            sDist = distV
+            color = 'green'
+        elif distV == 0:
+            canvas.create_line(app.px, app.py, xH, yH, fill = 'green', width = 2)
+            sDist = distH
+            color = 'green3'
+        elif distH > distV:
+            canvas.create_line(app.px, app.py, xV, yV, fill = 'green', width = 2)
+            sDist = distV
+            color = 'green'
         else:
-            yo = (app.px - rx)/(math.tan(ra-(3*math.pi/2)))
-        ry = (yo + app.py)
-        xo = app.mazeXO
-        
-    #looking Left
-    if ra > math.pi/2 or ra < (3 * math.pi/2):
-        rx = (int(app.px // app.mazeXO)) * app.mazeXO
-        if ra == math.pi:
-            yo = 0
-        elif ra < math.pi:
-            yo = (app.px - rx)/(math.tan(ra-math.pi/2))
-        else:
-            yo = (rx-app.px)*(math.tan(ra-math.pi))
+            canvas.create_line(app.px, app.py, xH, yH, fill = 'green', width = 2)
+            sDist = distH
+            color = 'green3'
 
-        ry = (yo + app.py)
-        xo = -app.mazeXO
+        #check bounds again after addition
+        ra += math.pi/180
+        if ra < 0:
+            ra += 2 * math.pi
+        if ra > 2 * math.pi:
+            ra -= 2 * math.pi
 
-    if ra == math.pi/2 or ra == 3* math.pi/2:
-        ry = app.py
-        rx = app.px
-        dof = app.dimensions
-    
-    while dof < app.dimensions:
-        rayRow = int(ry // app.mazeYO)
-        rayCol = int(rx // app.mazeXO)
+        #castAngle
+        ca = app.pa - ra
+        if ca < 0:
+            ca += 2 * math.pi
+        if ca > 2 * math.pi:
+            ca -= 2 * math.pi
 
-        if (rayRow < app.dimensions and rayCol < app.dimensions and 
-        app.maze[rayRow][rayCol] == 'w'):
-            dof = app.dimensions
-        else:
-            rx += xo
-            ry += yo
-            dof += 1
-    
-    canvas.create_line(app.px, app.py, rx, ry, fill = 'green', width = 1)
+        #cos on castAngle to fix fisheye effect
+        sDist = sDist * math.cos(ca)
+
+        #height of each individual ray on wall
+        height = (app.mazeXO * (340))/sDist
+
+        #line offset to center casted walls on screen
+        lineO = app.height/2 - height/2
+
+        #draw walls
+        canvas.create_line(ray * 8 + app.width/2, lineO, ray * 8 + app.height, 
+        height + lineO, fill = color, width = 8)
 
 def redrawAll(app, canvas):
+    drawBackground(app,canvas)
     drawMaze(app, canvas)
     drawPlayer(app, canvas)
     drawRays(app, canvas)
 
-runApp(width=800, height=800)
+runApp(width = 1600, height = 800)
+
+
+
 
 
 
